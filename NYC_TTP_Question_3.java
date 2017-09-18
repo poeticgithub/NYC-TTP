709b52794b5ed07b873fe6df403f31f5b804525d
Nujoma Halls <purplesunset2004@yahoo.com>
2017-09-18 15:53:17 -0400

    Optimized question 3

--- NYC_TTP_Question_3.java
+++ NYC_TTP_Question_3.java
@@ -7,7 +7,6 @@ public class NYC_TTP_Question_3 {
 		int openIndex = -1;
 		int closingIndex = -1;
 
-		Stack<Character> charStack = new Stack<Character>(); 
 		Stack<Integer> indicesStack = new Stack<Integer>(); //this stack will contain the indices of the opening parentheses
 
 		for (int i = 0; i < sentence.length(); i++) {
@@ -15,23 +14,21 @@ public class NYC_TTP_Question_3 {
 			char currentchar = sentence.charAt(i);
 
 			if (currentchar == '(') {
-				charStack.push(currentchar);
 				indicesStack.push(i);
 			}
 
 			else if (currentchar == ')') {
-				if (charStack.isEmpty() == false) {
-					charStack.pop();
+				if (indicesStack.isEmpty() == false) {
 					openIndex = indicesStack.pop();
 					if (openIndex == openingParenIndex)
 						closingIndex = i;
 				} 
-				else // if charStack is empty, return error value -1
+				else // if indicesStack is empty, return error value -1
 					return -1;
 			}
 		}// end for loop
 
-		if (charStack.isEmpty() == true)
+		if (indicesStack.isEmpty() == true)
 			return closingIndex; // returns closing paren index only if parentheses are balanced
 		else
 			return -1;
